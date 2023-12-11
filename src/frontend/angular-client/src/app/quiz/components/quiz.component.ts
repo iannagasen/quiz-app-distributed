5d4cd8bb25d8124a41ab4c102f9e98a875b03ef6
iannagasen <iann.agasen@gmail.com>
2023-12-11 14:07:34 +0800

    [AngularClient] - make quiz form template driven

--- src/frontend/angular-client/src/app/quiz/components/quiz.component.ts
+++ src/frontend/angular-client/src/app/quiz/components/quiz.component.ts
@@ -4,51 +4,65 @@ import { Quiz } from '../types/quiz';
 import { CommonModule } from '@angular/common';
 import { QuestionDTO } from '../types/question.dto';
 import { QuestionState } from '../types/question.state';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-quiz',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   templateUrl: './quiz.component.html', 
 })
 export class QuizComponent implements OnInit {
 
-
   questionsState: QuestionState[] = []
-
+  isSubmitted = false;
   topic: string = 'AWS'
+  scoreSummary: {
+    score: number,
+    unanswered: number,
+    total: number
+  } | undefined = undefined;
 
   constructor(
     private quizService: QuizService,
   ) { }
 
+
   ngOnInit(): void {
     this.questionsState = this.quizService.generateQuiz('AWS');
   }
 
+
   getRows(text: string): number {
     const length = text.length
-    console.log(`length is = ${length}`)
     if (length <= 90) return 1;
     if (length <= 180) return 2;
     return 3;
   }
 
 
-  isSelected(questionId: number, choiceId: number) {
-    const id = this.questionsState.find(q => q.id === questionId)?.selectedChoiceId
-    return id === choiceId;
+  onSubmit(form: NgForm) {
+    /**
+     * 1. show the banner with calculated score
+     * 2. TODO: call the api 
+     */
+    // console.log(object)
+    // console.log(form)
+    this.isSubmitted = true
+
+    this.scoreSummary = this.questionsState.reduce(
+      (result, question) => {
+        const selectedChoice = question.choices.find(c => c.id === question.selectedChoiceId);
+
+        if (selectedChoice?.correct) result.score ++;
+        if (!selectedChoice) result.unanswered ++;
+
+        return result;
+      },
+      { score: 0, unanswered: 0, total: this.questionsState.length}
+    )
   }
-  
-
-  selectAnswer(questionId: number, choiceId: number): void {
-    const question = this.questionsState.find(q => q.id === questionId);
-
-    if (question) {
-      question.selectedChoiceId = choiceId;
-    }
-  }
-
 }
